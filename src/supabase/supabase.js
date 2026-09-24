7244f7500c49663d3564b51882f76eb5f3ef0d51
Angel Chira <125518726+AngelChira@users.noreply.github.com>
2026-09-24 01:01:48 -0500

    Proyecto base con algunos paquetes para el desarrollo de la solución

---
+++ src/supabase/supabase.js
@@ -0,0 +1,14 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+
+const supabaseCliente = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    storage: localStorage,
+    persistSession: true,
+    autoRefreshToken: true,
+  },
+});
+
+export default supabaseCliente;
